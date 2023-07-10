3c65545e409770142de4c5d9bd56d40844034c42
BlackNight Shadow <65853201+BNLOS@users.noreply.github.com>
2023-07-10 07:08:21 +0300

    Update npmjs.js

--- npmjs.js
+++ npmjs.js
@@ -4,7 +4,7 @@
 // @match       https://www.npmjs.com/package/*
 // @match       https://unpkg.com/browse/*/dist/
 // @grant       GM_setClipboard
-// @version     1.2
+// @version     1.3
 // @author      hunmer
 // @description 2022/6/28 00:37:30
 // ==/UserScript==
@@ -12,67 +12,71 @@
 if (location.host == 'www.npmjs.com') {
   let span = document.querySelector('._50685029');
   let url = 'https://unpkg.com' + location.pathname.replace('package', 'browse') + '/dist/';
-  
+
   // Open link in new tab
   span.innerHTML = '<a href="' + url + '" target="_blank">' + span.innerHTML + '</a>';
 } else {
-  let [first, second, ...trs] = document.querySelectorAll('tr');
-  
-  for (let tr of trs) {
-    let btn = document.createElement('button');
-    
-    btn.onclick = e => {
-      let url = tr.querySelector('a').href.replace('/browse/', '/')
-      let ext = url.split('.').at(-1).toLowerCase();
-      let format = '{url}';
+  let filesTable = document.querySelector('div[data-testid="pkg-file-table"] table');
+  let tableRows = filesTable && filesTable.querySelectorAll('tbody > tr');
+
+  if (tableRows) {
+    tableRows.forEach((row) => {
+      let fileNameCell = row.querySelector('td:nth-child(2) a');
       
-      switch (ext) {
-        case 'css':
-          format = '<link href="{url}" rel="stylesheet" />';
-          break;
+      if (fileNameCell) {
+        let fileUrl = fileNameCell.href.replace('/browse/', '/');
+        let fileExtension = fileUrl.split('.').pop().toLowerCase();
+
+        let button = document.createElement('button');
+        button.innerText = 'Copy';
+        button.style.backgroundColor = '#f44336';
+        button.style.color = 'white';
+        button.style.border = 'none';
+        button.style.padding = '8px 12px';
+        button.style.borderRadius = '4px';
+        button.style.cursor = 'pointer';
+
+        button.addEventListener('click', () => {
+          let format = '{url}';
           
-        case 'js':
-          format = '<script src="{url}"></script>';
-          break;
+          switch (fileExtension) {
+            case 'css':
+              format = '<link href="{url}" rel="stylesheet" />';
+              break;
+              
+            case 'js':
+              format = '<script src="{url}"></script>';
+              break;
+
+            // Add support for additional file types
+            case 'png':
+              format = '<img src="{url}" alt="Image" />';
+              break;
+
+            case 'json':
+              format = '<script src="{url}"></script>';
+              break;
+          }
           
-        // Add support for additional file types
-        case 'png':
-          format = '<img src="{url}" alt="Image" />';
-          break;
-          
-        case 'json':
-          format = '<script src="{url}"></script>';
-          break;
+          GM_setClipboard(format.replace('{url}', fileUrl));
+
+          button.disabled = true;
+          button.innerText = 'Copied!';
+        });
+
+        let buttonCell = document.createElement('td');
+        buttonCell.appendChild(button);
+
+        row.appendChild(buttonCell);
       }
-      
-      GM_setClipboard(format.replace('{url}', url));
-      
-      // Provide copy notification
-      btn.innerText = 'Copied!';
-      btn.disabled = true;
-    }
-    
-    // Add a class to the button for styling
-    btn.className = 'copy-button';
-    
-    // Improved Button Styling
-    btn.style.backgroundColor = '#f44336';
-    btn.style.color = 'white';
-    btn.style.border = 'none';
-    btn.style.padding = '8px 12px';
-    btn.style.borderRadius = '4px';
-    btn.style.cursor = 'pointer';
-    
-    let td = document.createElement('td');
-    td.append(btn);
-    tr.append(td);
+    });
   }
 }
 
 // Keyboard Shortcut (Ctrl + C)
 document.addEventListener('keydown', function (event) {
   if (event.ctrlKey && event.code === "KeyC") {
-    let activeButton = document.querySelector('.copy-button');
+    let activeButton = document.querySelector('button[disabled]');
     if (activeButton) {
       activeButton.click();
     }
